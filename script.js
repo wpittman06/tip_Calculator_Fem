710966e97351cbaa3896090969cb80dcf678b676
William Pittman <wpittma05@yahoo.com>
2024-11-09 16:25:54 -0500

    Tip calculator project. It covers vanila HTML CSS and JS. Has you set the bill, how many people and the tip to calculate your portion of the bill (tip included)

---
+++ script.js
@@ -0,0 +1,84 @@
+const billInput = document.querySelector(".bill-input");
+const peopleInput = document.querySelector(".people-input");
+const tipPerPerson = document.getElementById("tip-amount");
+const totalPerPerson = document.getElementById("total-amount")
+const tips = document.querySelectorAll(".tip-container-item")
+const tipCustom = document.querySelector(".tip-custom")
+const resetBtn = document.querySelector(".calculator-reset-btn")
+const error = document.querySelector(".error-msg");
+
+billInput.addEventListener("input", billInputFun);
+peopleInput.addEventListener("input", peopleInputFun);
+tips.forEach(function (val) {
+    val.addEventListener("click", handleClick);
+});
+tipCustom.addEventListener("input", tipInputFun);
+resetBtn.addEventListener("click", reset)
+
+billInput.value = "0.0";
+peopleInput.value = "1";
+tipPerPerson.innerHTML = "$" + (0.0).toFixed(2);
+totalPerPerson.innerHTML = "$" + (0.0).toFixed(2);
+
+let billValue = 0.0;
+let peopleValue = 1;
+let tipValue = .15;
+
+function billInputFun(){
+    billValue = parseFloat(billInput.value)
+    calculateTip();
+}
+
+function peopleInputFun(){
+    peopleValue = parseFloat(peopleInput.value);
+
+    if(peopleValue < 1){
+        error.style.display = "flex";
+        peopleInput.style.border = "thick solid red"
+    }else {
+        error.style.display = 'none'
+        peopleInput.style.border = 'none'
+        calculateTip();
+    }
+}
+
+function tipInputFun() {
+    tipValue=parseFloat(tipCustom.value / 100);
+
+    tips.forEach(function (val) {
+        val.classList.remove("active-tip");
+    })
+    calculateTip();
+}
+
+function handleClick(event) {
+    tips.forEach(function (val){
+        val.classList.remove("active-tip");
+        if (event.target.innerHTML == val.innerHTML) {
+            val.classList.add("active-tip");
+            tipValue = parseFloat(val.innerHTML)/100
+        }
+    });
+    calculateTip();
+}
+
+
+function calculateTip(){
+    if(peopleValue >=1){
+        let tipAmount = (billValue * tipValue) / peopleValue
+        let total = (billValue / peopleValue) + tipAmount
+        tipPerPerson.innerHTML = "$" + tipAmount.toFixed(2);
+        totalPerPerson.innerHTML = "$" + total.toFixed(2);
+    }
+}
+
+function reset() {
+    billInput.value = "0.0";
+    peopleInput.value = "1";
+    tipCustom.value = "";
+    tips.forEach(val => val.classList.remove("active-tip"));
+    billValue = 0.0;
+    peopleValue = 1;
+    tipValue = 0.15;
+    calculateTip();
+}
